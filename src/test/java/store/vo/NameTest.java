a497debf61169e435b0da4e4eacc22393e9bef49
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 02:34:03 +0900

    test(NameTest): 테스트 코드 구현 완료
    TDD에 의거하여 테스트 코드를 먼저 작성했다.

---
+++ src/test/java/store/vo/NameTest.java
@@ -0,0 +1,38 @@
+package store.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameTest {
+
+    @Test
+    @DisplayName("이름은 null 값이 입력될 수 없다")
+    void nullNameTest() {
+        assertThatThrownBy(()-> new Name(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름으로 null값이 입력되었습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름은 빈칸이 들어갈 수 없다")
+    @ValueSource(strings = {"","  ","   ","\n","\t"})
+    void blankNameTest(String input) {
+        assertThatThrownBy(()-> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름으로 빈칸이 입력되었습니다.");
+    }
+
+    @Test
+    @DisplayName("이름은 지정된 최대 길이를 넘을 수 없다")
+    void nameLimitTest() {
+        assertThatThrownBy(()-> new Name("X".repeat(NAME_LIMIT+1)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름이 10글자를 초과했습니다");
+    }
+
+}

b3f28d8950c7d0db23456bddc86f86c471355259
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 03:59:34 +0900

    feat(ItemQuantity): 클래스 구현 완료

--- src/main/java/store/vo/ItemQuantity.java
+++ src/main/java/store/vo/ItemQuantity.java
@@ -4,9 +4,10 @@ import store.constants.Errors;
 
 public class ItemQuantity {
 
-    private final int itemQuantity;
+    private int itemQuantity;
 
     public ItemQuantity(int itemQuantity) {
+        isNotNegative(itemQuantity);
         this.itemQuantity = itemQuantity;
     }
 
@@ -14,9 +15,24 @@ public class ItemQuantity {
         return itemQuantity;
     }
 
-    private void isBiggerThanZero(int itemQuantity) {
-        if(itemQuantity <= 0) {
-            throw new IllegalArgumentException(Errors.PRICE_UNDER_ZERO.getMessage());
+    public void refill(int refillAmount) {
+        itemQuantity += refillAmount;
+    }
+
+    public void sell(int sellAmount) {
+        validateSellAmount(sellAmount);
+        itemQuantity -= sellAmount;
+    }
+
+    private void isNotNegative(int itemQuantity) {
+        if(itemQuantity < 0) {
+            throw new IllegalArgumentException(Errors.NEGATIVE_QUANTITY.getMessage());
+        }
+    }
+
+    private void validateSellAmount(int sellAmount) {
+        if(sellAmount > itemQuantity){
+            throw new IllegalArgumentException(Errors.OVER_QUANTITY_SELL.getMessage());
         }
     }
 }

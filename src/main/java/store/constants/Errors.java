88243fab232441fe4071b78c2c668997a0f4d875
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 03:34:05 +0900

    refactor(Errors): 물품 -> 상품 으로 이름 변경
    상품이 조금 더 직관적이라고 판단

--- src/main/java/store/constants/Errors.java
+++ src/main/java/store/constants/Errors.java
@@ -2,12 +2,12 @@ package store.constants;
 
 public enum Errors {
     //ItemName
-    NULL_NAME("품목의 이름으로 null값이 입력되었습니다."),
-    BLANK_NAME("품목의 이름으로 빈칸이 입력되었습니다."),
-    LONG_NAME("품목의 이름이 %d글자를 초과했습니다"),
+    NULL_NAME("상품의 이름으로 null값이 입력되었습니다."),
+    BLANK_NAME("상품의 이름으로 빈칸이 입력되었습니다."),
+    LONG_NAME("상품의 이름이 %d글자를 초과했습니다"),
 
     //ItemPrice
-    PRICE_UNDER_ZERO("품목의 가격은 0보다 커야합니다");
+    PRICE_UNDER_ZERO("상품의 가격은 0보다 커야합니다");
 
     private static final String ERROR_PREFIX = "[ERROR]";
 

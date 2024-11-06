3f40322c5567ad25797f53b524ad4815f95f4a45
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 02:59:58 +0900

    feat(ItemName): 클래스 구현 완료

---
+++ src/main/java/store/vo/ItemName.java
@@ -0,0 +1,42 @@
+package store.vo;
+
+import org.junit.platform.commons.util.StringUtils;
+import store.constants.Errors;
+
+import java.util.Objects;
+
+public class ItemName {
+
+    private static int NAME_LIMIT = 10;
+
+    private final String itemName;
+
+    public ItemName(String itemName) {
+        validateNull(itemName);
+        validateBlank(itemName);
+        validateLimit(itemName);
+        this.itemName = itemName;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    private void validateNull(String itemName) {
+        if (Objects.isNull(itemName)) {
+            throw new IllegalArgumentException(Errors.NULL_NAME.getMessage());
+        }
+    }
+
+    private void validateBlank(String itemName) {
+        if (StringUtils.isBlank(itemName)) {
+            throw new IllegalArgumentException(Errors.BLANK_NAME.getMessage());
+        }
+    }
+
+    private void validateLimit(String itemName) {
+        if (itemName.length() > NAME_LIMIT) {
+            throw new IllegalArgumentException(String.format(Errors.LONG_NAME.getMessage(), NAME_LIMIT));
+        }
+    }
+}

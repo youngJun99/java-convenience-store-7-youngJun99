c26b28048cbf8d0dad14733a218890826b7a801a
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 03:00:41 +0900

    refactor(ItemNameTest): Name -> ItemName으로 클래스명 리펙토링
    의미의 전달력을 더 높였음

--- src/test/java/store/vo/ItemNameTest.java
+++ src/test/java/store/vo/ItemNameTest.java
@@ -6,33 +6,35 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
-class NameTest {
+class ItemNameTest {
 
     @Test
     @DisplayName("이름은 null 값이 입력될 수 없다")
     void nullNameTest() {
-        assertThatThrownBy(()-> new Name(null))
+        assertThatThrownBy(() -> new ItemName(null))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("이름으로 null값이 입력되었습니다.");
+                .hasMessageContaining("품목의 이름으로 null값이 입력되었습니다.");
     }
 
     @ParameterizedTest
     @DisplayName("이름은 빈칸이 들어갈 수 없다")
-    @ValueSource(strings = {"","  ","   ","\n","\t"})
+    @ValueSource(strings = {"", "  ", "   ", "\n", "\t"})
     void blankNameTest(String input) {
-        assertThatThrownBy(()-> new Name(input))
+        assertThatThrownBy(() -> new ItemName(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("이름으로 빈칸이 입력되었습니다.");
+                .hasMessageContaining("품목의 이름으로 빈칸이 입력되었습니다.");
     }
 
+    /**
+     * Magic Number를 상수화 하고 싶었지만, NAME_LIMIT을 private으로 유지하는 것이 더 중요하다고 생각했습니다.
+     */
     @Test
     @DisplayName("이름은 지정된 최대 길이를 넘을 수 없다")
     void nameLimitTest() {
-        assertThatThrownBy(()-> new Name("X".repeat(NAME_LIMIT+1)))
+        assertThatThrownBy(() -> new ItemName("X".repeat(100)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("이름이 10글자를 초과했습니다");
+                .hasMessageContaining("품목의 이름이 10글자를 초과했습니다");
     }
 
 }

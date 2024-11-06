6cf8f746c0254b3ffe2119a452901498d7c738c2
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 03:24:27 +0900

    feat(Errors): Itemprice를 위한 에러 구현 완료

--- src/main/java/store/constants/Errors.java
+++ src/main/java/store/constants/Errors.java
@@ -1,10 +1,13 @@
 package store.constants;
 
 public enum Errors {
+    //ItemName
     NULL_NAME("품목의 이름으로 null값이 입력되었습니다."),
     BLANK_NAME("품목의 이름으로 빈칸이 입력되었습니다."),
-    LONG_NAME("품목의 이름이 %d글자를 초과했습니다");
+    LONG_NAME("품목의 이름이 %d글자를 초과했습니다"),
 
+    //ItemPrice
+    PRICE_UNDER_ZERO("품목의 가격은 0보다 커야합니다");
 
     private static final String ERROR_PREFIX = "[ERROR]";
 

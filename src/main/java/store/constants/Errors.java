419ae1bfbb4e12b3b4a7ab33f6962bf181569eb7
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 03:59:50 +0900

    feat(Errors): ItemQuantity관련 에러 추가

--- src/main/java/store/constants/Errors.java
+++ src/main/java/store/constants/Errors.java
@@ -7,7 +7,11 @@ public enum Errors {
     LONG_NAME("상품의 이름이 %d글자를 초과했습니다"),
 
     //ItemPrice
-    PRICE_UNDER_ZERO("상품의 가격은 0보다 커야합니다");
+    PRICE_UNDER_ZERO("상품의 가격은 0보다 커야합니다"),
+
+    //ItemQuantity
+    NEGATIVE_QUANTITY("상품의 개수는 음수일 수 없습니다"),
+    OVER_QUANTITY_SELL("상품의 재고가 충분하지 않습니다");
 
     private static final String ERROR_PREFIX = "[ERROR]";
 

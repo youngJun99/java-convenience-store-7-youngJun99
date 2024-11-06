0b6570a5ff0da84535db46d38581a33bf03a8175
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 03:24:00 +0900

    feat(ItemPrice): 클래스 구현 완료

---
+++ src/main/java/store/vo/ItemPrice.java
@@ -0,0 +1,17 @@
+package store.vo;
+
+public class ItemPrice {
+
+    private final int price;
+
+    public ItemPrice(int price) {
+        isBiggerThanZero(price);
+        this.price = price;
+    }
+
+    private void isBiggerThanZero(int price) {
+        if(price <= 0) {
+            throw new IllegalArgumentException("품목의 가격은 0보다 커야합니다");
+        }
+    }
+}

4263e3be50dcd81656651a8ad25e3a1ab7077f0a
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 03:24:52 +0900

    feat(ItemPrice): getter 구현

--- src/main/java/store/vo/ItemPrice.java
+++ src/main/java/store/vo/ItemPrice.java
@@ -9,6 +9,10 @@ public class ItemPrice {
         this.price = price;
     }
 
+    public int getPrice() {
+        return price;
+    }
+
     private void isBiggerThanZero(int price) {
         if(price <= 0) {
             throw new IllegalArgumentException("품목의 가격은 0보다 커야합니다");

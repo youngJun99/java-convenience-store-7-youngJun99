f13f40b966864496cdae9a29ae04088be0e9362e
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 03:33:58 +0900

    refactor(ItemNameTest): 물품 -> 상품 으로 이름 변경
    상품이 조금 더 직관적이라고 판단

--- src/test/java/store/vo/ItemNameTest.java
+++ src/test/java/store/vo/ItemNameTest.java
@@ -10,31 +10,31 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 class ItemNameTest {
 
     @Test
-    @DisplayName("이름은 null 값이 입력될 수 없다")
+    @DisplayName("상품의 이름은 null 값이 입력될 수 없다")
     void nullNameTest() {
         assertThatThrownBy(() -> new ItemName(null))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("품목의 이름으로 null값이 입력되었습니다.");
+                .hasMessageContaining("상품의 이름으로 null값이 입력되었습니다.");
     }
 
     @ParameterizedTest
-    @DisplayName("이름은 빈칸이 들어갈 수 없다")
+    @DisplayName("상품의 이름은 빈칸이 들어갈 수 없다")
     @ValueSource(strings = {"", "  ", "   ", "\n", "\t"})
     void blankNameTest(String input) {
         assertThatThrownBy(() -> new ItemName(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("품목의 이름으로 빈칸이 입력되었습니다.");
+                .hasMessageContaining("상품의 이름으로 빈칸이 입력되었습니다.");
     }
 
     /**
      * Magic Number를 상수화 하고 싶었지만, NAME_LIMIT을 private으로 유지하는 것이 더 중요하다고 생각했습니다.
      */
     @Test
-    @DisplayName("이름은 지정된 최대 길이를 넘을 수 없다")
+    @DisplayName("상품의 이름은 지정된 최대 길이를 넘을 수 없다")
     void nameLimitTest() {
         assertThatThrownBy(() -> new ItemName("X".repeat(100)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("품목의 이름이 10글자를 초과했습니다");
+                .hasMessageContaining("상품의 이름이 10글자를 초과했습니다");
     }
 
 }

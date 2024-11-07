efbfa6f625c533688bbbc5725a54ce722abcf267
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 01:09:11 +0900

    refactor(Errors): 이름에 관련한 에러 업데이트

--- src/main/java/store/constants/Errors.java
+++ src/main/java/store/constants/Errors.java
@@ -3,7 +3,7 @@ package store.constants;
 public enum Errors {
     //Name
     NULL_NAME("이름으로 null값이 입력되었습니다."),
-    BLANK_NAME("이름으로 빈칸이 입력되었습니다."),
+    BLANK_NAME("이름으로 빈칸이 입력되었습니다.");
 
 
     private static final String ERROR_PREFIX = "[ERROR]";

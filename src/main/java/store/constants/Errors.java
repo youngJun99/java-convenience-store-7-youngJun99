19af61eebeefe91c547301bc24d669e3cfcb7651
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 01:05:02 +0900

    fix(Errors): 에러 내용 삭제 및 재구성 시작
    고객 입장에서 재설계

--- src/main/java/store/constants/Errors.java
+++ src/main/java/store/constants/Errors.java
@@ -4,22 +4,7 @@ public enum Errors {
     //Name
     NULL_NAME("이름으로 null값이 입력되었습니다."),
     BLANK_NAME("이름으로 빈칸이 입력되었습니다."),
-    LONG_NAME("이름이 %d글자를 초과했습니다"),
 
-    //Money
-    NEGATIVE_MONEY("돈은 음수일 수 없습니다"),
-    OVER_MONEY_SUBTRACTION("돈을 음수로 만들 수 없습니다, 현재 돈은 %원 입니다."),
-
-    //Price
-    ZERO_PRICE("상품의 가격은 0보다 커야 합니다"),
-
-    //Quantity
-    NEGATIVE_QUANTITY("수량은 음수일 수 없습니다"),
-    OVER_QUANTITY_SUBTRACTION("수량을 음수로 만들 수 없습니다, 현재 수량은 %d개 입니다."),
-
-    //Product
-    NULL_IN_PRODUCT_CREATION("상품의 구성요소는 null값이 들어갈 수 없습니다"),
-    OVER_TOTAL_QUANTITY_REQUEST("%s의 재고인 %d개보다 많이 주문하셨습니다.");
 
     private static final String ERROR_PREFIX = "[ERROR]";
 

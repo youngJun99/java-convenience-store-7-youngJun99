2eb07c1fd321ac8b906558cc4745aeaef4dc5f69
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 00:29:11 +0900

    feat(Product): 생성 로직 및 매서드 뼈대 구현

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -7,6 +7,7 @@ import store.vo.Name;
 import store.vo.Money;
 import store.vo.Quantity;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Product {
@@ -18,12 +19,7 @@ public class Product {
     private final Quantity productQuantity;
 
     public Product(Name productName, Promotion promotion, Money productMoney, Quantity promotionQuantity, Quantity productQuantity) {
-        validateNull(productName);
-        validateNull(promotion);
-        validateNull(productMoney);
-        validateNull(promotionQuantity);
-        validateNull(productQuantity);
-        validateZeroPrice(productMoney.getPrice());
+        validateInputs(productName, promotion, productMoney, promotionQuantity, productQuantity);
         this.productName = productName;
         this.promotion = promotion;
         this.productMoney = productMoney;
@@ -31,6 +27,41 @@ public class Product {
         this.productQuantity = productQuantity;
     }
 
+
+    public ProductRequestDto checkRequestOf(int requestAmount, LocalDateTime timeofOrder) {
+        if(promotion.available(timeofOrder)) {
+            return checkPromotionRequest(requestAmount);
+        }
+       return checkNormalRequest(requestAmount);
+    }
+
+    private ProductRequestDto checkPromotionRequest(int requestAmount) {
+        return promotion.checkRequest(requestAmount,promotionQuantity,promotionQuantity);
+    }
+
+    private ProductRequestDto checkNormalRequest(int requestAmount) {
+
+    }
+
+    public void executeRequestOf(int executeAmount) {
+
+    }
+
+    private void validateRequest(int requestAmount) {
+        int TotalAmount = promotionQuantity.getQuantity()+productQuantity.getQuantity();
+        if(TotalAmount < requestAmount){
+            throw new IllegalArgumentException(Errors.OVER_TOTAL_QUANTITY_REQUEST.getMessage());
+        }
+    }
+
+    private void validateInputs(Name productName, Promotion promotion, Money productMoney, Quantity promotionQuantity, Quantity productQuantity) {
+        validateNull(productName);
+        validateNull(promotion);
+        validateNull(productMoney);
+        validateNull(promotionQuantity);
+        validateNull(productQuantity);
+        validateZeroPrice(productMoney.getPrice());
+    }
 
     private void validateZeroPrice(int price) {
         if(price == 0) {

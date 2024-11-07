e9d2eaaf692b4b58a37fd23e2db15f336446ebf8
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 22:42:58 +0900

    feat(Errors): 생성자 생성 로직 관련 에러 구현

--- src/main/java/store/constants/Errors.java
+++ src/main/java/store/constants/Errors.java
@@ -1,17 +1,24 @@
 package store.constants;
 
 public enum Errors {
-    //ItemName
+    //Name
     NULL_NAME("이름으로 null값이 입력되었습니다."),
     BLANK_NAME("이름으로 빈칸이 입력되었습니다."),
     LONG_NAME("이름이 %d글자를 초과했습니다"),
 
-    //ItemPrice
-    PRICE_UNDER_ZERO("가격은 0보다 커야합니다"),
+    //Money
+    NEGATIVE_MONEY("돈은 음수일 수 없습니다"),
+    OVER_MONEY_SUBTRACTION("돈을 음수로 만들 수 없습니다, 현재 돈은 %원 입니다."),
 
-    //ItemQuantity
+    //Price
+    ZERO_PRICE("상품의 가격은 0보다 커야 합니다"),
+
+    //Quantity
     NEGATIVE_QUANTITY("수량은 음수일 수 없습니다"),
-    OVER_QUANTITY_SUBTRACTION("수량을 음수로 만들 수 없습니다, 현재 수량은 %d개 입니다.");
+    OVER_QUANTITY_SUBTRACTION("수량을 음수로 만들 수 없습니다, 현재 수량은 %d개 입니다."),
+
+    //Product
+    NULL_IN_PRODUCT_CREATION("상품의 구성요소는 null값이 들어갈 수 없습니다");
 
     private static final String ERROR_PREFIX = "[ERROR]";
 

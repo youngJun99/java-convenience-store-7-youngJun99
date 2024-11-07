0478400c617dabef21dc3adc95e31bc439bbeb5c
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 05:50:08 +0900

    feat(Product): Order 프로모션 경우 제외 완료

--- src/main/java/store/domain/store/Product.java
+++ src/main/java/store/domain/store/Product.java
@@ -1,13 +1,14 @@
 package store.domain.store;
 
 import store.constants.InputErrors;
-import store.domain.OrderSheet;
 import store.domain.store.promotion.Promotion;
 import store.dto.CheckSummaryDto;
+import store.dto.ProductReceipt;
 
 import java.time.LocalDateTime;
 
 import static store.dto.CheckSummaryDto.normalSummaryFrom;
+import static store.dto.ProductReceipt.*;
 
 
 public class Product {
@@ -38,8 +39,24 @@ public class Product {
         return promotion.checkRequest(productName,requestAmount,promotionInventory);
     }
 
-    public void order(OrderSheet orderSheet) {
+    public ProductReceipt order(int requestAmount, LocalDateTime orderedTime) {
+        // 항상 check가 된 주문이 들어오지만, public으로 열려 있어 검증 추가
+        validateRequest(requestAmount);
+        if (promotion.available(orderedTime)) {
+            //구현 해야함
+            return
+        }
+        processNormalOrder(requestAmount);
+        return normalReceiptFrom(productName,price,requestAmount);
+    }
 
+    private void processNormalOrder(int requestAmount) {
+        if(normalInventory < requestAmount) {
+            requestAmount -= normalInventory;
+            normalInventory = 0;
+            promotionInventory -= requestAmount;
+        }
+        normalInventory -= requestAmount;
     }
 
     private void validateRequest(int requestAmount) {

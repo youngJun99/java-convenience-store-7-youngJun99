93c4327c4b6c22c9e3ebfa5fc1436dd4ae5f7132
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-07 22:42:32 +0900

    feat(Product): 생성자 및 생성 로직 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,5 +1,6 @@
 package store;
 
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,10 +1,13 @@
 package store.domain;
 
+import store.constants.Errors;
 import store.domain.promotion.Promotion;
 import store.dto.ProductRequestDto;
 import store.vo.Name;
 import store.vo.Money;
 import store.vo.Quantity;
+
+import java.util.Objects;
 
 public class Product {
 
@@ -15,6 +18,12 @@ public class Product {
     private final Quantity productQuantity;
 
     public Product(Name productName, Promotion promotion, Money productMoney, Quantity promotionQuantity, Quantity productQuantity) {
+        validateNull(productName);
+        validateNull(promotion);
+        validateNull(productMoney);
+        validateNull(promotionQuantity);
+        validateNull(productQuantity);
+        validateZeroPrice(productMoney.getPrice());
         this.productName = productName;
         this.promotion = promotion;
         this.productMoney = productMoney;
@@ -22,15 +31,17 @@ public class Product {
         this.productQuantity = productQuantity;
     }
 
-    public ProductRequestDto checkRequestOf(int requestAmount) {
-        return promotion.reviewRequest(productQuantity,requestAmount);
+
+    private void validateZeroPrice(int price) {
+        if(price == 0) {
+            throw new IllegalArgumentException(Errors.ZERO_PRICE.getMessage());
+        }
     }
 
-    public void executeRequestOf(int executeAmount) {
-
+    private void validateNull(Object object) {
+        if(Objects.isNull(object)){
+            throw new IllegalArgumentException(Errors.NULL_IN_PRODUCT_CREATION.getMessage());
+        }
     }
 
-    private validateZeroPrice(int price) {
-        if()
-    }
 }

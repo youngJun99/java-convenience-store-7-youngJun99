584b6f19a1b25edb839b91fd432c15e7849120f1
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 01:28:47 +0900

    refactor(Errors): 이름 변경

--- src/main/java/store/constants/Errors.java
+++ src/main/java/store/constants/Errors.java
@@ -2,7 +2,7 @@ package store.constants;
 
 public enum Errors {
 
-    PURCHASE_AMOUNT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
+    INVALID_AMOUNT_OR_NAME("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
     NO_SUCH_ITEM("존재하지 않는 상품입니다. 다시 입력해 주세요."),
     INVENTORY_SHORTAGE("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
     WRONG_INPUT("잘못된 입력입니다. 다시 입력해 주세요.");

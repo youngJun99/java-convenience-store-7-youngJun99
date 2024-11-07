ad0ef53369fad0c929c41e64e8936547485a69a4
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 04:33:33 +0900

    feat(PromotionImpl): 구현 완료

---
+++ src/main/java/store/domain/store/promotion/PromotionImpl.java
@@ -0,0 +1,61 @@
+package store.domain.store.promotion;
+
+import store.dto.CheckSummaryDto;
+
+import java.time.LocalDateTime;
+
+import static store.dto.CheckSummaryDto.*;
+
+public class PromotionImpl implements Promotion {
+
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDateTime start;
+    private final LocalDateTime end;
+
+    public PromotionImpl(String name, int buy, int get, LocalDateTime start, LocalDateTime end) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public boolean available(LocalDateTime orderedTime) {
+        return orderedTime.isAfter(start) && orderedTime.isBefore(end);
+    }
+
+    @Override
+    public CheckSummaryDto checkRequest(String productName, int requestAmount, int promotionInventory) {
+
+        int divider = buy + get;
+
+        int promotableAmount = divider * (promotionInventory / divider);
+
+        //requestAmount - promotableAmount 만큼 못준다
+        if (promotableAmount < requestAmount) {
+            return unPromotableFrom(productName, requestAmount - promotableAmount);
+        }
+        //프로모션을 문제없이 진행할 수 있다
+        if (promotableAmount == requestAmount) {
+            return normalSummaryFrom(productName);
+        }
+        if (promotableAmount > requestAmount) {
+            return checkCases(productName, requestAmount, divider, promotableAmount);
+        }
+    }
+
+    private CheckSummaryDto checkCases(String productName, int requestAmount, int divider, int promotableAmount) {
+        int leftOvers = requestAmount % divider;
+        //보너스를 줄 수 있다
+        if (leftOvers == buy && requestAmount + divider <= promotableAmount) {
+            return bonusReceivableFrom(productName, get);
+        }
+        //leftOver 만큼 보장해 줄 수 없다
+        if (leftOvers < buy) {
+            return unPromotableFrom(productName, leftOvers);
+        }
+    }
+}

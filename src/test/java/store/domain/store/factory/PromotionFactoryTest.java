0a73bf8cd9759ea3c64555d2da8bd743d9c74980
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-09 01:09:42 +0900

    test(PromotionFactoryTest):테스트 코드 작성 완료

---
+++ src/test/java/store/domain/store/factory/PromotionFactoryTest.java
@@ -0,0 +1,35 @@
+package store.domain.store.factory;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.domain.store.promotion.PromotionImpl;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+class PromotionFactoryTest {
+
+    private static List<PromotionImpl> answer;
+
+    @BeforeAll
+    static void setUp() {
+        PromotionImpl promotion1 = new PromotionImpl("탄산2+1", 2, 1, LocalDate.parse("2024-01-01"), LocalDate.parse("2024-12-31"));
+        PromotionImpl promotion2 = new PromotionImpl("MD추천상품", 1, 1, LocalDate.parse("2024-01-01"), LocalDate.parse("2024-12-31"));
+        PromotionImpl promotion3 = new PromotionImpl("반짝할인", 1, 1, LocalDate.parse("2024-11-01"), LocalDate.parse("2024-11-30"));
+        answer = List.of(promotion1, promotion2, promotion3);
+    }
+
+    @Test
+    @DisplayName("PromotionFactory 기능 테스트")
+    void promotionFactoryFunctionTest() throws IOException {
+        PromotionFactory promotionFactory = PromotionFactory.getInstance();
+        List<PromotionImpl> promotions = promotionFactory.loadPromotions("src/main/resources/promotions.md");
+        assertThat(promotions).isEqualTo(answer);
+    }
+
+}

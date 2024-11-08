9fcca3ef7a1f141cd299c53427495e854e8b911f
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 21:22:29 +0900

    chore(OrderDto): 주석 제거

--- src/main/java/store/dto/OrderDto.java
+++ src/main/java/store/dto/OrderDto.java
@@ -3,7 +3,6 @@ package store.dto;
 import store.vo.ItemName;
 import store.vo.PurchaseQuantity;
 
-//여기 값에 대한 validation을 만드는게 좋은 것 같음. Qunatitiy는 점검할 수 있을듯 이름은 나중에.
 public record OrderDto(
         ItemName itemName,
         PurchaseQuantity purchaseQuantity

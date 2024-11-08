03721ace4725814418ba39dcb38400dfff9c69e7
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-09 01:22:05 +0900

    refactor(ConvenienceStoreFactory): products 경로 상수화

--- src/main/java/store/domain/store/factory/ConvenienceStoreFactory.java
+++ src/main/java/store/domain/store/factory/ConvenienceStoreFactory.java
@@ -8,7 +8,6 @@ import store.domain.store.promotion.PromotionImpl;
 
 import java.util.List;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,22 +17,24 @@ import java.util.ArrayList;
 
 public class ConvenienceStoreFactory {
 
+    private static final String productsPath = "src/main/resources/products.md";
+
     private final List<PromotionImpl> promotions;
 
     public ConvenienceStoreFactory(List<PromotionImpl> promotions) {
         this.promotions = promotions;
     }
 
-    public ConvenienceStore createStore(String filePath) throws IOException {
+    public ConvenienceStore createStore() throws IOException {
         List<Product> products = new ArrayList<>();
-        Path path = Paths.get(filePath);
+        Path path = Paths.get(productsPath);
         extractProducts(path, products);
         return new ConvenienceStore(products);
     }
 
     private void extractProducts(Path path, List<Product> products) throws IOException {
         List<String> lines = Files.readAllLines(path);
-        for(int i=1; i<lines.size();i++) {
+        for (int i = 1; i < lines.size(); i++) {
             extractOneLine(lines.get(i), products);
         }
     }
@@ -45,13 +46,12 @@ public class ConvenienceStoreFactory {
         int promotionInventory = Integer.parseInt(data[2].trim());
         int normalInventory = Integer.parseInt(data[3].trim());
         Promotion promotion = NullPromotion.getInstance();
-        if(!data[4].trim().equals("null")) {
+        if (!data[4].trim().equals("null")) {
             promotion = getPromotionByName(data[4].trim());
         }
         products.add(new Product(productName, promotion, price, promotionInventory, normalInventory));
     }
 
-    // 프로모션 이름으로 Promotion 객체를 찾는 메소드
     private PromotionImpl getPromotionByName(String name) {
         return promotions.stream()
                 .filter(promotion -> promotion.getName().equals(name))

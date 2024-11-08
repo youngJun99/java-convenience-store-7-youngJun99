e91d3d013feeb5d1d28ee147a7f9d89eeec84a81
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 16:53:32 +0900

    feat(Purchase): 중점 매서드 구현 완료

--- src/main/java/store/domain/Purchase.java
+++ src/main/java/store/domain/Purchase.java
@@ -5,15 +5,15 @@ public class Purchase {
     private String productName;
     private Boolean approved;
     private int promotableAmount;
-    private int unpromotableAmount;
-    private int receivableBonus;
+    private int unPromotableAmount;
+    private int extraReceivableBonus;
 
-    private Purchase(String productName, Boolean approved, int promotableAmount, int unpromotableAmount, int receivableBonus) {
+    private Purchase(String productName, Boolean approved, int promotableAmount, int unPromotableAmount, int extraReceivableBonus) {
         this.productName = productName;
         this.approved = approved;
         this.promotableAmount = promotableAmount;
-        this.unpromotableAmount = unpromotableAmount;
-        this.receivableBonus = receivableBonus;
+        this.unPromotableAmount = unPromotableAmount;
+        this.extraReceivableBonus = extraReceivableBonus;
     }
 
     public static Purchase normalPurchaseFrom(String productName, int purchasedAmount) {
@@ -32,6 +32,34 @@ public class Purchase {
         return new Purchase(productName,false,promotedAmount,normalInventory,0);
     }
 
+    public String getProductName() {
+        return productName;
+    }
 
+    public int getPromotableAmount() {
+        return promotableAmount;
+    }
 
+    public int getUnPromotableAmount() {
+        return unPromotableAmount;
+    }
+
+    public int getExtraReceivableBonus() {
+        return extraReceivableBonus;
+    }
+
+    public Boolean isNotApproved() {
+        return !approved;
+    }
+
+    public void getBonus(){
+        approved = true;
+        promotableAmount =+extraReceivableBonus;
+        extraReceivableBonus = 0;
+    }
+
+    public void discardUnPromotableAmount() {
+        approved = true;
+        unPromotableAmount = 0;
+    }
 }

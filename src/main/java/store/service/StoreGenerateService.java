cba8e49eb86db5feea5fd9500dd572db14c8bf3e
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-09 03:24:24 +0900

    feat(StoreGenerateService): 구현 완료

--- src/main/java/store/service/StoreGenerateService.java
+++ src/main/java/store/service/StoreGenerateService.java
@@ -1,4 +1,16 @@
 package store.service;
 
+import store.domain.store.Store;
+import store.domain.store.factory.PromotionFactory;
+import store.domain.store.factory.StoreFactory;
+
+import java.io.IOException;
+
 public class StoreGenerateService {
+
+    public Store generateStore() throws IOException {
+        PromotionFactory promotionFactory = PromotionFactory.getInstance();
+        StoreFactory storeFactory = new StoreFactory(promotionFactory.loadPromotions());
+        return storeFactory.createStore();
+    }
 }

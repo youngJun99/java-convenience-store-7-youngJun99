1dfa69ec4f4a7304493ad86be3afdb0c7e024486
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 22:30:25 +0900

    feat(PromotionFactory): 구현 완료

---
+++ src/main/java/store/domain/store/factory/PromotionFactory.java
@@ -0,0 +1,42 @@
+package store.domain.store.factory;
+
+import store.domain.store.promotion.NullPromotion;
+import store.domain.store.promotion.Promotion;
+import store.domain.store.promotion.PromotionImpl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromotionFactory {
+
+    public static List<Promotion> loadPromotions(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        List<Promotion> promotions = new ArrayList<>();
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            // 첫 줄을 건너뜁니다.
+            reader.readLine();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                String name = data[0].trim();
+                int buy = Integer.parseInt(data[1].trim());
+                int get = Integer.parseInt(data[2].trim());
+                LocalDate startDate = LocalDate.parse(data[3].trim());
+                LocalDate endDate = LocalDate.parse(data[4].trim());
+
+                promotions.add(new PromotionImpl(name, buy, get, startDate, endDate));
+            }
+        }
+        promotions.add(new NullPromotion());
+        return promotions;
+    }
+}
+

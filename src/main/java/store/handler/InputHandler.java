35c66ab22072c63a34ee45c0045c91338750db80
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-09 03:11:39 +0900

    feat(InputHandler): 구현 완료

---
+++ src/main/java/store/handler/InputHandler.java
@@ -0,0 +1,50 @@
+package store.handler;
+
+import store.dto.OrderDto;
+import store.dto.OrderSheetDto;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputHandler {
+
+    private static final Pattern orderPattern = Pattern.compile("\\[(\\w+)-(\\d+)]");
+
+    private InputValidator inputValidator;
+
+    public InputHandler(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public OrderSheetDto handleOrder(String inputOrder) {
+        inputValidator.validateProductOrder(inputOrder);
+
+        //promotion.md의 형태에 따라 우테코 라이브러리를 사용하지 않았습니다.
+        LocalDate orderTime = LocalDate.now();
+
+        List<OrderDto> orders = extractOrders(inputOrder);
+
+        return new OrderSheetDto(orders, orderTime);
+
+    }
+
+    public boolean handleCustomerResponse(String inputResponse) {
+        inputValidator.validateCustomerResponse(inputResponse);
+        return inputResponse.equals("Y");
+    }
+
+    private static List<OrderDto> extractOrders(String inputOrder) {
+        List<OrderDto> orders = new ArrayList<>();
+        Matcher matcher = orderPattern.matcher(inputOrder);
+        while (matcher.find()) {
+            String productName = matcher.group(1);
+            int quantity = Integer.parseInt(matcher.group(2));
+            orders.add(new OrderDto(productName, quantity));
+        }
+        return orders;
+    }
+
+}

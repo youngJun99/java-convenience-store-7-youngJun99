e56d815b28732d083a40363fd5b3d8d967fb6b52
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 20:40:43 +0900

    feat(ShoppingCart): processResponses 구현

---
+++ src/main/java/store/domain/ShoppingCart.java
@@ -0,0 +1,37 @@
+package store.domain;
+
+import store.dto.OrderApproveRequestDto;
+import store.dto.OrderApproveResponseDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ShoppingCart {
+
+    List<Purchase> purchases;
+
+    public ShoppingCart(List<Purchase> purchases) {
+        this.purchases = purchases;
+    }
+
+    public boolean isAllApproved() {
+        return purchases.stream()
+                .allMatch(Purchase::getApproved);
+    }
+
+    public List<OrderApproveRequestDto> printUnApprovedPurchases() {
+        return purchases.stream()
+                .filter(purchase -> !purchase.getApproved()) // 승인되지 않은 Purchase만 필터링
+                .map(purchase -> purchase.makeRequest()) // 각 Purchase에서 makeRequest() 호출하여 OrderApproveRequestDto 생성
+                .collect(Collectors.toList()); // 결과를 리스트로 반환
+    }
+
+    public void processResponses(List<OrderApproveResponseDto> responses) {
+        responses.forEach(response ->
+                purchases.stream()
+                        .filter(purchase -> purchase.getProductName().equals(response.productName()))
+                        .findFirst()
+                        .ifPresent(purchase -> purchase.processResponse(response))
+        );
+    }
+}

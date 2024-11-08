365a43a647c6c5086f24b61c35a103125aa97d91
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 20:08:14 +0900

    feat(ConvenienceStore): 구현 완료

--- src/main/java/store/domain/store/ConvenienceStore.java
+++ src/main/java/store/domain/store/ConvenienceStore.java
@@ -1,13 +1,53 @@
 package store.domain.store;
 
+import store.constants.InputErrors;
+import store.domain.Order;
+import store.domain.OrderSheet;
+import store.domain.Purchase;
+import store.domain.ShoppingCart;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ConvenienceStore {
 
-    List<Product> products;
+    private final List<Product> products;
 
     public ConvenienceStore(List<Product> products) {
+
         this.products = products;
     }
+
+    public ShoppingCart putInCart(OrderSheet orderSheet) {
+        validateOrderProducts(orderSheet);
+        LocalDateTime orderedTime = orderSheet.orderedTime();
+        return new ShoppingCart(orderSheet.orders().stream()
+                .map(order -> {
+                    return getPurchase(order, orderedTime);
+                })
+                .toList());
+
+    }
+
+    private Purchase getPurchase(Order order, LocalDateTime orderedTime) {
+        Product matchingProduct = products.stream()
+                .filter(product -> product.getProductName().equalsIgnoreCase(order.itemName().getName()))
+                .findFirst()
+                .get();
+
+        return matchingProduct.makePendingPurchase(order.purchaseQuantity().getQuantity(), orderedTime);
+    }
+
+
+    private void validateOrderProducts(OrderSheet orderSheet) {
+        boolean allItemsAvailable = orderSheet.orders().stream()
+                .allMatch(order -> products.stream()
+                        .anyMatch(product -> product.getProductName().equals(order.itemName().getName()))
+                );
+        if (!allItemsAvailable) {
+            throw new IllegalArgumentException(InputErrors.NO_SUCH_ITEM.getMessage());
+        }
+    }
+
 
 }

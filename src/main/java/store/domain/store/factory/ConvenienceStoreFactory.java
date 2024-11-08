848dcfda8445f93c5e80f3211264cd2fc941081c
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-09 00:15:54 +0900

    feat(ConvenienceStoreFactory): 구현 완료

---
+++ src/main/java/store/domain/store/factory/ConvenienceStoreFactory.java
@@ -0,0 +1,64 @@
+package store.domain.store.factory;
+
+import store.domain.store.ConvenienceStore;
+import store.domain.store.Product;
+import store.domain.store.promotion.NullPromotion;
+import store.domain.store.promotion.Promotion;
+import store.domain.store.promotion.PromotionImpl;
+
+import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+public class ConvenienceStoreFactory {
+
+    private final List<PromotionImpl> promotions;
+
+    public ConvenienceStoreFactory(List<PromotionImpl> promotions) {
+        this.promotions = promotions;
+    }
+
+    public ConvenienceStore createStore(String filePath) throws IOException {
+        List<Product> products = new ArrayList<>();
+        Path path = Paths.get(filePath);
+        extractProducts(path, products);
+        return new ConvenienceStore(products);
+    }
+
+    private void extractProducts(Path path, List<Product> products) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            // 첫 줄 스킵
+            reader.readLine();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                extractOneLine(line, products);
+            }
+        }
+    }
+
+    private void extractOneLine(String line, List<Product> products) {
+        String[] data = line.split(",");
+        String productName = data[0].trim();
+        int price = Integer.parseInt(data[1].trim());
+        int promotionInventory = Integer.parseInt(data[2].trim());
+        int normalInventory = Integer.parseInt(data[3].trim());
+        Promotion promotion = NullPromotion.getInstance();
+        if(!data[4].trim().equals("null")) {
+            promotion = getPromotionByName(data[4].trim());
+        }
+        products.add(new Product(productName, promotion, price, promotionInventory, normalInventory));
+    }
+
+    // 프로모션 이름으로 Promotion 객체를 찾는 메소드
+    private PromotionImpl getPromotionByName(String name) {
+        return promotions.stream()
+                .filter(promotion -> promotion.getName().equals(name))
+                .findFirst().get();
+    }
+}

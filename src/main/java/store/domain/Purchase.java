932f9f3a8f3f195200242c6b5f36cd23ff79ab6d
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 21:32:21 +0900

    refactor(Purchase): price를 필드에서 제거
    영수증 생성을 편의점에서 할 예정이기 때문에 price 필드 필요없음

--- src/main/java/store/domain/Purchase.java
+++ src/main/java/store/domain/Purchase.java
@@ -6,35 +6,33 @@ import store.dto.OrderApproveResponseDto;
 public class Purchase {
 
     private String productName;
-    private int price;
     private Boolean approved;
     private int promotableAmount;
     private int unPromotableAmount;
     private int extraReceivableBonus;
 
-    public Purchase(String productName, int price, Boolean approved, int promotableAmount, int unPromotableAmount, int extraReceivableBonus) {
+    public Purchase(String productName, Boolean approved, int promotableAmount, int unPromotableAmount, int extraReceivableBonus) {
         this.productName = productName;
-        this.price = price;
         this.approved = approved;
         this.promotableAmount = promotableAmount;
         this.unPromotableAmount = unPromotableAmount;
         this.extraReceivableBonus = extraReceivableBonus;
     }
 
-    public static Purchase normalPurchaseFrom(String productName, int price, int purchasedAmount) {
-        return new Purchase(productName, price, true, 0, purchasedAmount, 0);
+    public static Purchase normalPurchaseFrom(String productName, int purchasedAmount) {
+        return new Purchase(productName, true, 0, purchasedAmount, 0);
     }
 
-    public static Purchase fullPromotablePurchaseFrom(String productName, int price, int purchasedAmount) {
-        return new Purchase(productName, price, true, purchasedAmount, 0, 0);
+    public static Purchase fullPromotablePurchaseFrom(String productName, int purchasedAmount) {
+        return new Purchase(productName, true, purchasedAmount, 0, 0);
     }
 
-    public static Purchase bonusReceivablePurchaseFrom(String productName, int price, int purchaseAmount, int receivableBonus) {
-        return new Purchase(productName, price, false, purchaseAmount, 0, receivableBonus);
+    public static Purchase bonusReceivablePurchaseFrom(String productName, int purchaseAmount, int receivableBonus) {
+        return new Purchase(productName, false, purchaseAmount, 0, receivableBonus);
     }
 
-    public static Purchase partialPromotablePurchaseFrom(String productName, int price, int promotedAmount, int normalInventory) {
-        return new Purchase(productName, price, false, promotedAmount, normalInventory, 0);
+    public static Purchase partialPromotablePurchaseFrom(String productName, int promotedAmount, int normalInventory) {
+        return new Purchase(productName, false, promotedAmount, normalInventory, 0);
     }
 
     public String getProductName() {

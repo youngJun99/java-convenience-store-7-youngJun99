a3966ad0d05fd6fedf213f238b8c92aa886f2014
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-08 14:13:54 +0900

    refactor(PromotionImpl): 오버라이딩 매서드 제거
    인터페이스 단에서 없애고 업데이트 하지 못했다

--- src/main/java/store/domain/store/promotion/PromotionImpl.java
+++ src/main/java/store/domain/store/promotion/PromotionImpl.java
@@ -58,14 +58,4 @@ public class PromotionImpl implements Promotion {
 
     }
 
-    @Override
-    public int calculatePromoted(int requestAmount, int promotionInventory) {
-        int divider = buy+get;
-
-        if(requestAmount > promotionInventory) {
-            return promotionInventory/divider*get;
-        }
-        return requestAmount/divider*get;
-    }
-
 }

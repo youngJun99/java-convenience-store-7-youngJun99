42fd081070c5f28f7a1776a85f67af534a3e5e77
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 06:17:10 +0900

    refactor(InputView): 오타 수정

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -5,7 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
 
     public String printOrderRequest() {
-        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예 : [사이다 -2],[감자칩-1]");
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예 : [사이다-2],[감자칩-1]");
         return readLine();
     }
 

6def928def62392ab5b0e57413ddc468994ded3e
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-09 22:39:19 +0900

    refactor(InputHandler): Inputview를 주입받아서 매서드를 실행하도록 리펙토링

--- src/main/java/store/handler/InputHandler.java
+++ src/main/java/store/handler/InputHandler.java
@@ -2,6 +2,7 @@ package store.handler;
 
 import store.dto.OrderDto;
 import store.dto.OrderSheetDto;
+import store.view.InputView;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -14,12 +15,15 @@ public class InputHandler {
     private static final Pattern orderPattern = Pattern.compile("\\[(\\w+)-(\\d+)]");
 
     private InputValidator inputValidator;
+    private InputView inputView;
 
-    public InputHandler(InputValidator inputValidator) {
+    public InputHandler(InputValidator inputValidator, InputView inputView) {
         this.inputValidator = inputValidator;
+        this.inputView = inputView;
     }
 
-    public OrderSheetDto handleOrder(String inputOrder) {
+    public OrderSheetDto requestInputOrder() {
+        String inputOrder = inputView.printOrderRequest();
         inputValidator.validateProductOrder(inputOrder);
 
         //promotion.md의 형태에 따라 우테코 라이브러리의 LocalTimes.now 를 사용하지 않았습니다.
@@ -28,15 +32,34 @@ public class InputHandler {
         List<OrderDto> orders = extractOrders(inputOrder);
 
         return new OrderSheetDto(orders, orderTime);
-
     }
 
-    public boolean handleCustomerResponse(String inputResponse) {
+    public boolean requestWhetherToReceiveBonus(String productName, int receivableBonus) {
+        String inputAnswer = inputView.printExtraBonusReceiveRequest(productName, receivableBonus);
+        return handleCustomerResponse(inputAnswer);
+    }
+
+    public boolean requestToConfirmUnPromotable(String productName, int unPromotable) {
+        String inputAnswer = inputView.printUnPromotableConditionRequest(productName, unPromotable);
+        return handleCustomerResponse(inputAnswer);
+    }
+
+    public boolean requestMemberShipDiscount() {
+        String inputAnswer = inputView.printMemberShipDiscountRequest();
+        return handleCustomerResponse(inputAnswer);
+    }
+
+    public boolean requestContinueShopping() {
+        String inputAnswer = inputView.printExtraBuyRequest();
+        return handleCustomerResponse(inputAnswer);
+    }
+
+    private boolean handleCustomerResponse(String inputResponse) {
         inputValidator.validateCustomerResponse(inputResponse);
         return inputResponse.equals("Y");
     }
 
-    private static List<OrderDto> extractOrders(String inputOrder) {
+    private List<OrderDto> extractOrders(String inputOrder) {
         List<OrderDto> orders = new ArrayList<>();
         Matcher matcher = orderPattern.matcher(inputOrder);
         while (matcher.find()) {

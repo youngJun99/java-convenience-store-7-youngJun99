184eee9bcc7421358dce3ebabd235ba95607a46f
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-09 20:48:57 +0900

    refactor(PromotionFactoryImpl): loadPromotions를 오버라이딩 받는 매서드로 변경

--- src/main/java/store/factory/PromotionFactoryImpl.java
+++ src/main/java/store/factory/PromotionFactoryImpl.java
@@ -11,10 +11,11 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PromotionFactoryImpl {
+public class PromotionFactoryImpl implements PromotionFactory{
 
     private static final String promotionPath = "src/main/resources/promotions.md";
 
+    @Override
     public List<Promotion> loadPromotions() throws IOException {
         List<Promotion> promotions = new ArrayList<>();
 

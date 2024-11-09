84ec401f0ec3d98610beb69e5cd7123a1f92f596
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-09 20:59:02 +0900

    feat(StoreGenerateService): 구현 완료

--- src/main/java/store/service/StoreGenerateService.java
+++ src/main/java/store/service/StoreGenerateService.java
@@ -1,25 +1,19 @@
 package store.service;
 
 import store.domain.store.Store;
-import store.domain.store.promotion.PromotionImpl;
-import store.factory.PromotionFactoryImpl;
-import store.factory.StoreFactoryImpl;
+import store.factory.StoreFactory;
 
 import java.io.IOException;
-import java.util.List;
 
 public class StoreGenerateService {
 
-    private final StoreFactoryImpl storeFactoryImpl;
-    private final PromotionFactoryImpl promotionFactoryImpl;
+    private final StoreFactory storeFactory;
 
-    public StoreGenerateService(StoreFactoryImpl storeFactoryImpl, PromotionFactoryImpl promotionFactoryImpl) {
-        this.storeFactoryImpl = storeFactoryImpl;
-        this.promotionFactoryImpl = promotionFactoryImpl;
+    public StoreGenerateService(StoreFactory storeFactory) {
+        this.storeFactory = storeFactory;
     }
 
     public Store generateStore() throws IOException {
-        List<PromotionImpl> promotions = promotionFactoryImpl.loadPromotions();
-        return storeFactoryImpl.createStore(promotions);
+        return storeFactory.createStore();
     }
 }

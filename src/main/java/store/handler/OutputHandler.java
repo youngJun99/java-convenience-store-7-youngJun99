f98e6f2c594f532a54de9bd8d3704e45731962c0
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-09 21:50:56 +0900

    feat(OutputHandler): 클래스 생성

---
+++ src/main/java/store/handler/OutputHandler.java
@@ -0,0 +1,12 @@
+package store.handler;
+
+import store.view.OutputView;
+
+public class OutputHandler {
+
+    private final OutputView outputView;
+
+    public OutputHandler(OutputView outputView) {
+        this.outputView = outputView;
+    }
+}

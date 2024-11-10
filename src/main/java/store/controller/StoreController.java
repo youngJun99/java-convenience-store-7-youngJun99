d90366aed84759557c10cbd0b2bf63d241aca44f
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 22:33:42 +0900

    feat(StoreController): 클래스 구현 완료

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,7 +1,35 @@
 package store.controller;
 
+import store.domain.Purchase;
+import store.domain.store.Store;
+import store.dto.ReceiptDto;
+import store.service.StoreGenerateService;
+import store.service.StoreOrderService;
+import store.service.StoreRecieptService;
+
+import java.io.IOException;
+import java.util.List;
+
 public class StoreController {
 
+    private final StoreGenerateService storeGenerateService;
+    private final StoreOrderService storeOrderService;
+    private final StoreRecieptService storeRecieptService;
 
+    public StoreController(StoreGenerateService storeGenerateService, StoreOrderService storeOrderService, StoreRecieptService storeRecieptService) {
+        this.storeGenerateService = storeGenerateService;
+        this.storeOrderService = storeOrderService;
+        this.storeRecieptService = storeRecieptService;
+    }
+
+    public void run() {
+        Store store = storeGenerateService.generateStore();
+        boolean keepBuying = true;
+        while (keepBuying){
+            List<ReceiptDto> receiptDtos = storeOrderService.takeOrderFrom(store);
+            storeRecieptService.printReceipt(receiptDtos);
+            keepBuying = storeOrderService.keepShopping();
+        }
+    }
 
 }

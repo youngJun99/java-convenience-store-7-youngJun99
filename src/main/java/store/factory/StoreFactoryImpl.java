afbb632a0d6fddd9577ebdb6889cd8b55c71f98e
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 17:25:43 +0900

    refactor(StoreFactory): 원본 product 형식에 맞게 작동하도록 리펙토링

--- src/main/java/store/factory/StoreFactoryImpl.java
+++ src/main/java/store/factory/StoreFactoryImpl.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 public class StoreFactoryImpl implements StoreFactory {
 
 
-
     private final PromotionFactory promotionFactory;
 
     private List<Promotion> promotions;
@@ -37,21 +36,31 @@ public class StoreFactoryImpl implements StoreFactory {
     private void extractProducts(Path path, List<Product> products) throws IOException {
         List<String> lines = Files.readAllLines(path);
         for (int i = 1; i < lines.size(); i++) {
-            extractOneLine(lines.get(i), products);
-        }
-    }
 
-    private void extractOneLine(String line, List<Product> products) {
-        String[] data = line.split(",");
-        String productName = data[0].trim();
-        int price = Integer.parseInt(data[1].trim());
-        int promotionInventory = Integer.parseInt(data[2].trim());
-        int normalInventory = Integer.parseInt(data[3].trim());
-        Promotion promotion = NullPromotion.getInstance();
-        if (!data[4].trim().equals("null")) {
-            promotion = getPromotionByName(data[4].trim());
+            String[] data = lines.get(i).split(",");
+            String productName = data[0].trim();
+            int price = Integer.parseInt(data[1].trim());
+
+            if (data[3].trim().equals("null")) {
+                int normalInventory = Integer.parseInt(data[2].trim());
+                Promotion noPromotion = NullPromotion.getInstance();
+                products.add(new Product(productName,noPromotion,price,0,normalInventory));
+                continue;
+            }
+            int promotionInventory = Integer.parseInt(data[2].trim());
+            Promotion promotion = getPromotionByName(data[3].trim());
+
+            if (i + 1 < lines.size()) {
+                String[] nextLine = lines.get(i + 1).split(",");
+                if (nextLine[0].trim().equals(productName)) {
+                    int normalInventory = Integer.parseInt(nextLine[2].trim());
+                    products.add(new Product(productName, promotion, price, promotionInventory, normalInventory));
+                    i++;
+                    continue;
+                }
+            }
+            products.add(new Product(productName, promotion, price, promotionInventory, 0));
         }
-        products.add(new Product(productName, promotion, price, promotionInventory, normalInventory));
     }
 
     private Promotion getPromotionByName(String name) {

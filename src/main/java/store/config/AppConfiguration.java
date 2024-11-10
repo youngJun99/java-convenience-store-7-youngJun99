9524ebdb9b8801d41109802593271c5e51c9f719
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 19:53:06 +0900

    feat(AppConfiguration): config 구현 완료

--- src/main/java/store/config/AppConfiguration.java
+++ src/main/java/store/config/AppConfiguration.java
@@ -1,29 +1,43 @@
 package store.config;
 
-import store.dto.ReceiptDto;
+import store.controller.StoreController;
 import store.factory.PromotionFactoryImpl;
 import store.factory.StoreFactoryImpl;
 import store.handler.InputHandler;
 import store.handler.InputValidator;
+import store.handler.OutputHandler;
 import store.service.StoreGenerateService;
 import store.service.StoreOrderService;
+import store.service.StoreRecieptService;
+import store.view.InputView;
+import store.view.OutputView;
 
-//서비스와 필요한 Validator를 박아서 주자
 public class AppConfiguration {
 
+    public StoreController storeController() {
+        InputHandler inputHandler = new InputHandler(new InputValidator(), new InputView());
+        OutputHandler outputHandler = new OutputHandler(new OutputView());
+        return storeController(inputHandler, outputHandler);
+    }
 
-
-
+    private StoreController storeController(InputHandler inputHandler, OutputHandler outputHandler) {
+        StoreGenerateService storeGenerateService = storeGenerateService();
+        StoreOrderService storeOrderService = storeOrderService(inputHandler, outputHandler);
+        StoreRecieptService storeRecieptService = storeRecieptService(outputHandler);
+        return new StoreController(storeGenerateService, storeOrderService, storeRecieptService);
+    }
 
     private StoreGenerateService storeGenerateService() {
         return new StoreGenerateService(new StoreFactoryImpl(new PromotionFactoryImpl()));
     }
 
-    private StoreOrderService storeOrderService() {
-        return  new StoreOrderService(new InputHandler(new InputValidator()));
+    private StoreOrderService storeOrderService(InputHandler inputHandler, OutputHandler outputHandler) {
+        return new StoreOrderService(inputHandler, outputHandler);
     }
 
-
+    private StoreRecieptService storeRecieptService(OutputHandler outputHandler) {
+        return new StoreRecieptService(outputHandler);
+    }
 
 
 }

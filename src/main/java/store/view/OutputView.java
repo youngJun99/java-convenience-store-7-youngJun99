09264e5199c1027d88a90ee6ebbdd6c8c0ed54e5
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 19:00:12 +0900

    refactor(OutputView): 뷰 형식에 맞게 리펙토링

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -16,30 +16,30 @@ public class OutputView {
 
     public void printBoughtInventory(List<String> buyAmount) {
         System.out.println("==============W 편의점================");
-        System.out.println("상품명\t\t수량\t금액");
+        System.out.println("상품명\t\t\t\t수량\t\t금액");
         buyAmount.forEach(System.out::println);
     }
 
     public void printBonusAmount(List<String> bonusAmount) {
-        System.out.println("=============증\t정===============");
+        System.out.println("=============증\t\t정===============");
         bonusAmount.forEach(System.out::println);
         System.out.println("====================================");
     }
 
     public void printTotalPrice(int totalAmount, int totalPrice) {
-        System.out.printf("총구매액\t\t%d\t%d%n",totalAmount,totalPrice);
+        System.out.printf("총구매액\t\t\t\t%d\t\t%d%n",totalAmount,totalPrice);
     }
 
     public void printPromotionDiscount(int discount) {
-        System.out.printf("행사할인\t\t\t-%d%n",discount);
+        System.out.printf("행사할인\t\t\t\t\t\t-%d%n",discount);
     }
 
     public void printMemberShipDiscount(int discount) {
-        System.out.printf("멤버십할인\t\t\t-%d%n",discount);
+        System.out.printf("멤버십할인\t\t\t\t\t\t-%d%n",discount);
     }
 
     public void printFinalPayAmount(int total) {
-        System.out.printf("내실돈\t\t\t %d%n",total);
+        System.out.printf("내실돈\t\t\t\t\t\t %d%n",total);
         System.out.println();
     }
 

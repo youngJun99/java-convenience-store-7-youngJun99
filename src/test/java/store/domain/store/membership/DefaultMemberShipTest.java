7c452b17a42d8afbb07861b0154c613dcc78ec3f
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 22:18:11 +0900

    test(DefaultMemberShipTest): 테스트 코드 작성 완료

---
+++ src/test/java/store/domain/store/membership/DefaultMemberShipTest.java
@@ -0,0 +1,39 @@
+package store.domain.store.membership;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultMemberShipTest {
+
+    private static DefaultMemberShip defaultMemberShip;
+
+    @BeforeAll
+    static void setUp() {
+        defaultMemberShip = new DefaultMemberShip();
+    }
+
+    @ParameterizedTest
+    @DisplayName("26666원의 30%는 8000원 입니다. 이 이상의 값이 입력되면 8000원을 반환합니다.")
+    @ValueSource(ints = {27000, 30000, 50000})
+    void overDiscountMaximumTest(int input) {
+        assertThat(defaultMemberShip.processDiscount(input)).isEqualTo(8000);
+    }
+
+    @ParameterizedTest
+    @DisplayName("맴버십은 26666원 이하의 값들에 대해서는 정확한 할인가격을 반환합니다.")
+    @CsvSource({
+            "25000, 7500",
+            "20000, 6000",
+            "15000, 4500"
+    })
+    void test(int input, int expect) {
+        assertThat(defaultMemberShip.processDiscount(input)).isEqualTo(expect);
+    }
+}

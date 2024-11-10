9c2fe7d80503ddbf7931f48926006ac2531050d7
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 19:47:51 +0900

    feat(StoreReceiptService): 클래스 생성

---
+++ src/main/java/store/service/StoreRecieptService.java
@@ -0,0 +1,12 @@
+package store.service;
+
+import store.handler.OutputHandler;
+
+public class StoreRecieptService {
+
+    private final OutputHandler outputHandler;
+
+    public StoreRecieptService(OutputHandler outputHandler) {
+        this.outputHandler = outputHandler;
+    }
+}

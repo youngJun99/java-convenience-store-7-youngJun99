263d80da29d37a735e984bc1e4e56996bc090b34
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 20:16:19 +0900

    feat(InputErrors): 재고와 프로모션 입력과정 에러 추가

--- src/main/java/store/constants/InputErrors.java
+++ src/main/java/store/constants/InputErrors.java
@@ -5,7 +5,10 @@ public enum InputErrors {
     INVALID_AMOUNT_OR_NAME("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
     NO_SUCH_ITEM("존재하지 않는 상품입니다. 다시 입력해 주세요."),
     INVENTORY_SHORTAGE("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
-    WRONG_INPUT("잘못된 입력입니다. 다시 입력해 주세요.");
+    WRONG_INPUT("잘못된 입력입니다. 다시 입력해 주세요."),
+
+    PROMOTION_READ_ERROR("프로모션 입력과정에서 에러가 발생했습니다."),
+    PRODUCT_READ_ERROR("재고 입력과정에서 에러가 발생했습니다.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
 

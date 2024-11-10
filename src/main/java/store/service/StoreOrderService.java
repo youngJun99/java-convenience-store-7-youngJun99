e7baa012db96e37781751673a06b50a3abc37ff4
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 21:10:35 +0900

    feat(StoreOrderService): 구현 완료

---
+++ src/main/java/store/service/StoreOrderService.java
@@ -0,0 +1,41 @@
+package store.service;
+
+import store.domain.ShoppingCart;
+import store.domain.store.Store;
+import store.dto.OrderApproveRequestDto;
+import store.dto.OrderApproveResponseDto;
+import store.dto.OrderDto;
+import store.dto.ReceiptDto;
+import store.handler.InputHandler;
+import store.handler.OutputHandler;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+public class StoreOrderService {
+
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
+
+    public StoreOrderService(InputHandler inputHandler, OutputHandler outputHandler) {
+        this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
+    }
+
+
+   public List<ReceiptDto> takeOrderFrom(Store store) {
+        List<OrderDto> customerOrder = inputHandler.requestInputOrder();
+        LocalDate orderedTime = LocalDate.now();
+        ShoppingCart purchases = store.putInCart(customerOrder,orderedTime);
+        if(purchases.isNotApproved()) {
+            List<OrderApproveRequestDto> unapproved = purchases.getUnApprovedPurchases();
+            List<OrderApproveResponseDto> response = unapproved.stream()
+                    .map(inputHandler::requestApproval)
+                    .toList();
+            purchases.processResponses(response);
+        }
+        return store.executeOrder(purchases,orderedTime);
+   }
+
+}

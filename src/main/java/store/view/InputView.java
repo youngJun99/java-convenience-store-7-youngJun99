5b0ed8a608582c6167eec14998cda914779794b1
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-11 04:53:55 +0900

    chore(InputView): 코드 포맷 정리

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -10,12 +10,12 @@ public class InputView {
     }
 
     public String printExtraBonusReceiveRequest(String productName, int receivableBonus) {
-        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n",productName,receivableBonus);
+        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n", productName, receivableBonus);
         return readLine();
     }
 
     public String printUnPromotableConditionRequest(String productName, int unPromotable) {
-        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n",productName,unPromotable);
+        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n", productName, unPromotable);
         return readLine();
     }
 

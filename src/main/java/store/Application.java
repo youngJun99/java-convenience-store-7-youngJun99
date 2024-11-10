f1b94ee220c3d3c904f2d8d4202a2ceaa5b39084
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 20:54:35 +0900

    refactor(Application): 미사용 임포테이션 제거

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -4,8 +4,6 @@ package store;
 import store.config.AppConfiguration;
 import store.controller.StoreController;
 
-import java.io.IOException;
-
 public class Application {
     public static void main(String[] args) {
         AppConfiguration appConfig = new AppConfiguration();

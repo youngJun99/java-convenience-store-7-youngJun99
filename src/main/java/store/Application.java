eb23597dd98e57848fc39973c85c13353e83f907
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 20:25:16 +0900

    feat(Application): 어플리케이션 로직 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -4,8 +4,10 @@ package store;
 import store.config.AppConfiguration;
 import store.controller.StoreController;
 
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         AppConfiguration appConfig = new AppConfiguration();
 
         StoreController controller = appConfig.storeController();

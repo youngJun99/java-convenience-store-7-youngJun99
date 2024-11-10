9e0192767ad8ed30f96cb4f9e456d53e1d2ac5d5
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-11 04:47:22 +0900

    feat(InputErrors): 최대 재입력 횟수에 대한 에러 구현

--- src/main/java/store/constants/InputErrors.java
+++ src/main/java/store/constants/InputErrors.java
@@ -7,7 +7,8 @@ public enum InputErrors {
     INVENTORY_SHORTAGE("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
     WRONG_INPUT("잘못된 입력입니다. 다시 입력해 주세요."),
 
-    STORE_BUILDING_ERROR("재고와 프로모션을 읽어오는 과정에서 에러가 발생했습니다.");
+    STORE_BUILDING_ERROR("재고와 프로모션을 읽어오는 과정에서 에러가 발생했습니다."),
+    EXCEED_MAX_RETRY_LIMIT("최대 재입력 횟수를 초과했습니다. 어플리케이션이 종료됩니다.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
 

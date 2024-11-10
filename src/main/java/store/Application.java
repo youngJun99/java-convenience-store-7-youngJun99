d7d01f247fac8c301ae9ad68ffd4043e0cb9d3d2
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 19:55:19 +0900

    feat(Application): 구현 완료

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,15 @@
 package store;
 
 
+import store.config.AppConfiguration;
+import store.controller.StoreController;
+
 public class Application {
     public static void main(String[] args) {
+        AppConfiguration appConfig = new AppConfiguration();
+
+        StoreController controller = appConfig.storeController();
+
+        controller.run();
     }
 }

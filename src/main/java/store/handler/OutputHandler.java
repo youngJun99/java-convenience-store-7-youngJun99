035cb7b1d6bae291e9babedf7a8f5ab330d9e129
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-10 18:44:40 +0900

    feat(OutputHandler): 영수증 출력 로직 구현

--- src/main/java/store/handler/OutputHandler.java
+++ src/main/java/store/handler/OutputHandler.java
@@ -1,6 +1,7 @@
 package store.handler;
 
 import store.dto.ProductInventoryDto;
+import store.dto.ReceiptDto;
 import store.view.OutputView;
 
 import java.text.DecimalFormat;
@@ -9,8 +10,7 @@ import java.util.List;
 
 public class OutputHandler {
 
-    private static final DecimalFormat MONEY_FORMAT = new DecimalFormat("#,###원");
-    private static final DecimalFormat INVENTORY_FORMAT = new DecimalFormat("#개");
+    private static final DecimalFormat MONEY_FORMAT = new DecimalFormat("#,###");
 
     private final OutputView outputView;
 
@@ -26,22 +26,65 @@ public class OutputHandler {
             if(!inventory.promotionName().isBlank()) {
                 String productName = inventory.productName();
                 String price = MONEY_FORMAT.format(inventory.price());
-                String promotionInventory = INVENTORY_FORMAT.format(inventory.promotionInventory());
-                String normalInventory = INVENTORY_FORMAT.format(inventory.normalInventory());
+                int promotionInventory = inventory.promotionInventory();
+                int normalInventory =inventory.normalInventory();
                 inventoryList.add(formatInventoryInterface(productName,price,promotionInventory,inventory.promotionName()));
                 inventoryList.add(formatInventoryInterface(productName,price,normalInventory,""));
                 continue;
             }
             String productName = inventory.productName();
             String price = MONEY_FORMAT.format(inventory.price());
-            String normalInventory = INVENTORY_FORMAT.format(inventory.normalInventory());
+            int normalInventory = inventory.normalInventory();
             inventoryList.add(formatInventoryInterface(productName,price,normalInventory,""));
         }
         outputView.printInventory(inventoryList);
     }
 
-    private String formatInventoryInterface(String productName, String price, String inventory, String promotionName){
-        return String.format("%s %s %s %s",productName,price,inventory,promotionName);
+    private String formatInventoryInterface(String productName, String price, int inventory, String promotionName){
+        return String.format("%s %s원 %d개 %s",productName,price,inventory,promotionName);
+    }
+
+    public void printReceipt(List<ReceiptDto> receipts, int memberShipDiscount) {
+
+        List<String> purchasedInventory = new ArrayList<>();
+        List<String> bonusReceived = new ArrayList<>();
+        int totalBoughtInventory = 0;
+        int totalPrice = 0;
+        int discountPrice = 0;
+
+
+        for(ReceiptDto receipt:receipts) {
+            String productName = receipt.productName();
+            int price = receipt.price();
+            if(receipt.promotionBonus() != 0){
+                int bonusAmount = receipt.promotionBonus();
+                discountPrice += bonusAmount*price;
+                bonusReceived.add(formatBonusReceived(productName,bonusAmount));
+            }
+            int boughtAmount = receipt.buyAmount();
+            int boughtPrice = boughtAmount*price;
+            totalBoughtInventory+=boughtAmount;
+            totalPrice += boughtPrice;
+            String boughtTotal = MONEY_FORMAT.format(boughtPrice);
+            purchasedInventory.add(formatBoughtInventory(productName,boughtAmount,boughtTotal));
+        }
+
+        int finalPayment = totalPrice-discountPrice-memberShipDiscount;
+
+        outputView.printBoughtInventory(purchasedInventory);
+        outputView.printBonusAmount(bonusReceived);
+        outputView.printTotalPrice(totalBoughtInventory,totalPrice);
+        outputView.printPromotionDiscount(discountPrice);
+        outputView.printMemberShipDiscount(memberShipDiscount);
+        outputView.printFinalPayAmount(finalPayment);
+    }
+
+    private String formatBoughtInventory(String productName, int boughtInventory, String boughtPrice) {
+        return String.format("%s        %d  %s",productName,boughtInventory,boughtPrice);
+    }
+
+    private String formatBonusReceived(String productName, int bonusReceived) {
+        return String.format("%s            %d",productName,bonusReceived);
     }
 
 
